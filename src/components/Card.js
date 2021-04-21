13a4eba106e23838e78e8bc1653019fc3ebe2586
Vladimir Pochtarenko <wooldik94@gmail.com>
2021-04-21 19:00:25 +0300

    add rules

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,32 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { useHistory } from "react-router";
 import { classNameBootstrap, routes } from "../constants";
 import Appearance from "./Person/Appearance";
 import { S } from "./styledComponents";
 
+// eslint-disable-next-line react/prop-types
 function Card({ data }) {
 
 	const history = useHistory();
 	console.log(data);
-	function backHandler () {
+	function backHandler() {
 		history.push(routes.mainpage);
 	}
 	return (<S.M10>
-		<button 
-		type="button" 
-		className={classNameBootstrap.buttonLighte}
-		onClick={backHandler}
+		<button
+			type="button"
+			className={classNameBootstrap.buttonLighte}
+			onClick={backHandler}
 		>Back</button>
 
 		<S.Wrapper>
 			<S.ImageFullHeight>
-				<img src={data.image.url} alt={data.name}/>
+				<img src={data.image.url} alt={data.name} />
 			</S.ImageFullHeight>
 			<S.Biograph>
 				<h2>{data.name}</h2>
-				<Appearance appearance={data.appearance}/>
+				<Appearance appearance={data.appearance} />
 			</S.Biograph>
 		</S.Wrapper>
 	</S.M10>);
